d9ffacf8baa521fca137ba7f2e8b5158963b8f7d
Figueroa, Santiago <sfigueroa@ceit.es>
2023-12-05 18:39:34 +0100

    update pipelines with development

--- development/main.tf
+++ development/main.tf
@@ -89,8 +89,6 @@ resource "proxmox_vm_qemu" "test_server" {
       "sudo apt-get install -y docker.io",
       "sudo systemctl enable docker",
       "sudo usermod -aG docker ${USER}",
-      "sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
-      "sudo chmod +x /usr/local/bin/docker-compose",
     ]
   }
 }
